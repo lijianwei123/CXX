97719b522e07dc6671bda822ad0061cd8c189255
lijianwei123 <442885811@qq.com>
2014-10-27 14:32:39 +0800

    提交玩玩

---
+++ index.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include "mongo/client/dbclient.h"
+
+void run()
+{
+	mongo::DBClientConnection c;
+	c.connect("localhost");
+}
+
+int main()
+{
+	mongo::client::initialize();
+	try	{
+		run();
+		std::cout << "connected ok" << std::endl;
+
+	} catch( const mongo::DBException &e) {
+		std::cout << "caught" << e.what() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
