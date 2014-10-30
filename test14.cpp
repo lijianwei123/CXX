6986868f04a7701b2a25d5f181720ff33983602d
lijianwei123 <442885811@qq.com>
2014-10-30 10:49:49 +0800

    更新

--- test14.cpp
+++ test14.cpp
@@ -1,3 +1,4 @@
+//验证一些预定义的宏
 #include <iostream>
 
 using namespace std;
@@ -5,5 +6,6 @@ using namespace std;
 int main()
 {
 	cout << "date:" << __DATE__ << "time:" << __TIME__ << endl;
+	cout << "file:" << __FILE__ << "line:" << __LINE__ << "function:" << __FUNCTION__ << endl;
 	return 0;
 }
---
+++ test15.cpp
@@ -0,0 +1,31 @@
+//验证系统自带的hash_map
+//
+#include <iostream>
+#include <ext/hash_map>
+
+using namespce __gnu_cxx;
+using namespace std;
+
+namespace __gnu_cxx
+{
+	template<> struct hash<string>
+	{
+		size_t operator()(const string& s) const
+		{
+			return __stl_hash_stirng(s.c_str());
+		}
+	};
+
+}
+
+int main(int argc, char *argv[])
+{
+	hash_map<string, int> hm;
+	hm.insert(pair<string, int>(string("aaa"), 123));
+	hash_map<string, int>::iterator it;
+	it = hm.find(string("aaa"));
+	cout << "value:" << it->second << endl;
+
+	
+	return 0;
+}
