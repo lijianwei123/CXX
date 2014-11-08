78aab7438f49c1d41a06b73cf72d7ba34dfe90a8
root <root@localhost.lab>
2014-11-08 13:37:57 +0800

    no message

---
+++ test19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <vector>
+
+using namespace std;
+using namespace boost;
+
+
+int main(int argc, char *argv[])
+{
+	shared_ptr<vector<int> > spv = make_shared<vector<int> >(10, 2);
+
+	return 0;
+}
