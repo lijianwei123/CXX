21a287836a68fc8959b97cce5d51f0304992c5f3
root <root@localhost.lab>
2014-11-08 14:31:24 +0800

    no message

--- test19.cpp
+++ test19.cpp
@@ -1,4 +1,3 @@
-//g++ -g -Wall index.c -o index
 #include <iostream>
 #include <stdio.h>
 #include <boost/make_shared.hpp>
@@ -13,6 +12,11 @@ using namespace boost;
 int main(int argc, char *argv[])
 {
 	shared_ptr<vector<int> > spv = make_shared<vector<int> >(10, 2);
+	vector<int>::iterator it;
+	for (it = spv->begin(); it != spv->end(); ++it) {
+		cout << *it << endl;
+	}	
+	
 
 	return 0;
 }
