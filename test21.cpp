dc482672270bb39d971f581c34878e7af8c75fec
root <root@localhost.lab>
2014-11-10 13:33:03 +0800

    no message

---
+++ test21.cpp
@@ -0,0 +1,58 @@
+//验证共享内存（引用指针)
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include <errno.h>
+#include <sys/wait.h>
+
+
+using namespace std;
+
+typedef struct {
+	char *name;
+} shm_shared_t;
+
+int main()
+{
+
+	key_t ipc_shm_key = ftok("/dev/null", (int)'a');
+	int shm_id = 0;
+	shm_id = shmget(ipc_shm_key, sizeof(shm_shared_t), IPC_CREAT|0666);
+	
+	shm_shared_t *shm_ptr = (shm_shared_t *)shmat(shm_id, NULL, 0);
+	const char *name = "lijianwei";
+	shm_ptr->name = const_cast<char *>(name);
+
+	pid_t pid = 0;
+	pid = fork();
+	if (pid == 0) {
+		shm_shared_t *shm_ptr = (shm_shared_t *)shmat(shm_id, NULL, 0);
+		printf("shm_ptr->name: %s\n", shm_ptr->name);
+		shm_ptr->name = const_cast<char *>("weiyanping");
+		sleep(10);
+		exit(EXIT_SUCCESS); 
+	}
+
+	pid = fork();
+	if (pid == 0) {	
+		shm_shared_t *shm_ptr = (shm_shared_t *)shmat(shm_id, NULL, 0);
+                printf("shm_ptr->name: %s\n", shm_ptr->name);
+		sleep(10);
+		exit(EXIT_SUCCESS);
+	}
+	
+	int status = 0;
+	pid_t cPid = 0;
+	while ((cPid = wait(&status)) > 0) {
+		printf("child pid: %d done\n", cPid);
+		printf("shm_ptr->name:%s\n", shm_ptr->name);
+	}
+	
+	return 0;
+}
