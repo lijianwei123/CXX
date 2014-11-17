4f13dc232ad50c3941d89e953b5a409b2033bb92
root <root@localhost.lab>
2014-11-17 14:43:13 +0800

    no message

---
+++ test26.cpp
@@ -0,0 +1,42 @@
+//g++ -g -Wall  test26.cpp -o test26
+//output
+//create
+//1
+//2
+//delete
+
+#include <iostream>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+class test
+{
+public:
+	test()
+	{
+		cout << "create" << endl;
+	}
+	~test()
+	{
+		cout << "delete" << endl;
+	}
+
+};
+
+int main()
+{
+	shared_ptr<test> test2_ptr;
+	shared_ptr<test> test_ptr = make_shared<test>();
+	cout << test_ptr.use_count() << endl;
+	test2_ptr = test_ptr;	
+	cout << test2_ptr.use_count() << endl;
+
+	return 0;
+	
+
+}
